e1b3526cb0162b139867cd25ad7783814a9f28d3
GAHEE KIM <hishally@naver.com>
2022-04-30 17:35:26 +0900

    [refactor] 줄맞춤

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,13 +1,14 @@
 package baseball;
 
 //import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 import java.util.Random;
 
 public class GameController {
 
-    private static final int MIN_NUMBER=1;
-    private static final int MAX_NUMBER=9;
-    private static final int NUMBER_RANGE=MAX_NUMBER-MIN_NUMBER;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int NUMBER_RANGE = MAX_NUMBER - MIN_NUMBER;
     private static final int ANSWER_LENGTH = 3;
 
     private boolean keepPlayingGame = true;
@@ -31,76 +32,76 @@ public class GameController {
 
         while (!isCorrectAnswer) {
             String userAnswer = ioController.getUserAnswer();
-            isCorrectAnswer=checkUserAnswer(realAnswer,userAnswer);
+            isCorrectAnswer = checkUserAnswer(realAnswer, userAnswer);
         }
     }
 
     private void askKeepPlayingGame() {
-        keepPlayingGame= ioController.getUserReply();
+        keepPlayingGame = ioController.getUserReply();
     }
 
     private String getRealAnswerByRandom() {
-        boolean[] existence=new boolean[NUMBER_RANGE];
-        String realAnswer="";
+        boolean[] existence = new boolean[NUMBER_RANGE];
+        String realAnswer = "";
 
-        for(int i=0;i<ANSWER_LENGTH;++i){
-            int randomNumber=pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
-            if(existence[randomNumber-MIN_NUMBER]){
+        for (int i = 0; i < ANSWER_LENGTH; ++i) {
+            int randomNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (existence[randomNumber - MIN_NUMBER]) {
                 i--;
                 continue;
             }
 
-            existence[randomNumber-MIN_NUMBER]=true;
-            realAnswer+=Integer.toString(randomNumber);
+            existence[randomNumber - MIN_NUMBER] = true;
+            realAnswer += Integer.toString(randomNumber);
         }
 
         return realAnswer;
     }
 
-    private boolean checkUserAnswer(String realAnswer,String userAnswer){
-        int strikeCount=0;
-        int ballCount=0;
+    private boolean checkUserAnswer(String realAnswer, String userAnswer) {
+        int strikeCount = 0;
+        int ballCount = 0;
 
-        for(int i=0;i<ANSWER_LENGTH;++i){
-            if(isStrike(realAnswer.charAt(i),userAnswer.charAt(i))){
+        for (int i = 0; i < ANSWER_LENGTH; ++i) {
+            if (isStrike(realAnswer.charAt(i), userAnswer.charAt(i))) {
                 strikeCount++;
                 continue;
             }
-            if(isBall(realAnswer,userAnswer.charAt(i))){
+            if (isBall(realAnswer, userAnswer.charAt(i))) {
                 ballCount++;
             }
         }
 
-        ioController.announceTurnResult(strikeCount,ballCount);
+        ioController.announceTurnResult(strikeCount, ballCount);
 
-        return isCorrectAnswer(strikeCount,ballCount);
+        return isCorrectAnswer(strikeCount, ballCount);
     }
 
-    private boolean isStrike(char realChar, char userChar){
-        return realChar==userChar;
+    private boolean isStrike(char realChar, char userChar) {
+        return realChar == userChar;
     }
 
-    private boolean isBall(String realAnswer, char userChar){
-        for(int i=0;i<ANSWER_LENGTH;++i){
-            if(realAnswer.charAt(i)==userChar){
+    private boolean isBall(String realAnswer, char userChar) {
+        for (int i = 0; i < ANSWER_LENGTH; ++i) {
+            if (realAnswer.charAt(i) == userChar) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isCorrectAnswer(int strikeCount, int ballCount){
-        return (strikeCount==ANSWER_LENGTH && ballCount==0);
+    private boolean isCorrectAnswer(int strikeCount, int ballCount) {
+        return (strikeCount == ANSWER_LENGTH && ballCount == 0);
     }
 
-    private int pickNumberInRange(int min, int max){
-        if(min>max) {
-            int tmp=min;
-            min=max;
-            max=tmp;
+    private int pickNumberInRange(int min, int max) {
+        if (min > max) {
+            int tmp = min;
+            min = max;
+            max = tmp;
         }
 
-        Random random=new Random();
-        return random.nextInt(max-min+1)+min;
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
 }
--- src/main/java/baseball/IOController.java
+++ src/main/java/baseball/IOController.java
@@ -1,6 +1,7 @@
 package baseball;
 
 //import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,12 +13,12 @@ public class IOController {
 
     private static int ANSWER_LENGTH = 3;
 
-    private static final String GET_USER_ANSWER_MESSAGE="숫자를 입력해주세요 : ";
-    private static final String GET_USER_REPLY_MESSAGE="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String KEEP_PLAYING_GAME="1";
-    private static final String STOP_PLAYING_GAME="2";
+    private static final String GET_USER_ANSWER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GET_USER_REPLY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String KEEP_PLAYING_GAME = "1";
+    private static final String STOP_PLAYING_GAME = "2";
 
-    public IOController(){
+    public IOController() {
     }
 
     public IOController(int answerLength) {
@@ -27,76 +28,77 @@ public class IOController {
     public String getUserAnswer() {
         System.out.print(GET_USER_ANSWER_MESSAGE);
 
-        String userAnswer="";
-        try{
-            userAnswer=br.readLine();
+        String userAnswer = "";
+        try {
+            userAnswer = br.readLine();
             checkUserAnswerInputError(userAnswer);
-        } catch(Exception e){
+        } catch (Exception e) {
             occurInputError();
-        };
+        }
+        ;
 
         return userAnswer;
     }
 
 
-    public boolean getUserReply(){
+    public boolean getUserReply() {
         System.out.println(GET_USER_REPLY_MESSAGE);
 
-        String userReply="";
-        try{
-            userReply=br.readLine();
+        String userReply = "";
+        try {
+            userReply = br.readLine();
             checkUserReplyInputError(userReply);
-        } catch(Exception e){
+        } catch (Exception e) {
             occurInputError();
         }
 
-        return (userReply==KEEP_PLAYING_GAME);
+        return (userReply == KEEP_PLAYING_GAME);
     }
 
-    public void announceTurnResult(int strikeCount, int ballCount){
+    public void announceTurnResult(int strikeCount, int ballCount) {
         String turnResultMessage = "";
 
-        if(strikeCount==0 && ballCount==0){
-            turnResultMessage="낫싱";
+        if (strikeCount == 0 && ballCount == 0) {
+            turnResultMessage = "낫싱";
         }
-        if(ballCount>0){
-            turnResultMessage+=Integer.toString(ballCount)+"볼 ";
+        if (ballCount > 0) {
+            turnResultMessage += Integer.toString(ballCount) + "볼 ";
         }
-        if(strikeCount>0 && strikeCount<ANSWER_LENGTH){
-            turnResultMessage+=Integer.toString(strikeCount)+"스트라이크 ";
+        if (strikeCount > 0 && strikeCount < ANSWER_LENGTH) {
+            turnResultMessage += Integer.toString(strikeCount) + "스트라이크 ";
         }
-        if(strikeCount==ANSWER_LENGTH){
-           turnResultMessage=Integer.toString(ANSWER_LENGTH) + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        if (strikeCount == ANSWER_LENGTH) {
+            turnResultMessage = Integer.toString(ANSWER_LENGTH) + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
         }
 
         System.out.println(turnResultMessage);
     }
 
-    private void checkUserAnswerInputError(String userAnswer){
-        if(userAnswer.length()!=ANSWER_LENGTH){
+    private void checkUserAnswerInputError(String userAnswer) {
+        if (userAnswer.length() != ANSWER_LENGTH) {
             occurInputError();
         }
 
-        for(int i=0;i<ANSWER_LENGTH;++i){
-            if(isNotNaturalNumber(userAnswer.charAt(i))){
+        for (int i = 0; i < ANSWER_LENGTH; ++i) {
+            if (isNotNaturalNumber(userAnswer.charAt(i))) {
                 occurInputError();
             }
         }
     }
 
-    private void checkUserReplyInputError(String userReply){
-        if(userReply==KEEP_PLAYING_GAME || userReply==STOP_PLAYING_GAME){
+    private void checkUserReplyInputError(String userReply) {
+        if (userReply == KEEP_PLAYING_GAME || userReply == STOP_PLAYING_GAME) {
             return;
         }
         occurInputError();
 
     }
 
-    private static boolean isNotNaturalNumber(char c){
+    private static boolean isNotNaturalNumber(char c) {
         return (c < '1' || c > '9');
     }
 
-    private static void occurInputError(){
+    private static void occurInputError() {
         throw new IllegalArgumentException();
     }
 
