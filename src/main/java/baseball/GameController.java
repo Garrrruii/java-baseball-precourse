8a67645ad3d109ce47215989e3c6d54d527d83b5
GAHEE KIM <hishally@naver.com>
2022-04-30 16:41:40 +0900

    [feat] gamePlay, inputController

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -2,26 +2,47 @@ package baseball;
 
 public class GameController {
 
-    private boolean keepPlayingGame=true;
+    private boolean keepPlayingGame = true;
+    private int ANSWER_LENGTH = 3;
+
+    private InputController inputController = new InputController(ANSWER_LENGTH);
 
     public GameController() {
     }
 
-    public void controlGame(){
+    public void controlGame() {
 
-        while(keepPlayingGame){
+        while (keepPlayingGame) {
             playGame();
             askKeepPlayingGame();
         }
     }
 
-    private void playGame(){
+    private void playGame() {
+        String realAnswer = getRealAnswerByRandom();
+        boolean isCorrectAnswer = false;
 
+        while (!isCorrectAnswer) {
+            String userAnswer = inputController.getUserAnswer();;
+            isCorrectAnswer=checkUserAnswer();
+        }
     }
 
-    private void askKeepPlayingGame(){
 
+    private void askKeepPlayingGame() {
+        keepPlayingGame=inputController.getUserReply();
     }
 
+    private String getRealAnswerByRandom() {
+        return "123";
+    }
 
+    private boolean checkUserAnswer(){
+
+        return false;
+    }
+
+    private boolean isStrike(char realChar, char userChar){
+        return realChar==userChar;
+    }
 }
